733360448ee34e99d223bb4555d46740ec4ac99e
Franklinsalvius <31512783+Franklinsalvius@users.noreply.github.com>
2025-07-02 16:44:14 +0530

    Create COBOL_FIXED_LENGTH_ATTRIBUTES.sql

---
+++ COBOL_FIXED_LENGTH_ATTRIBUTES.sql
@@ -0,0 +1,8 @@
+CREATE TABLE COBOL_FIXED_LENGTH_ATTRIBUTES (
+    OperationName VARCHAR(100),
+    ProgramName VARCHAR(100),
+    Request_payload1 TEXT,
+    Request_payload2 TEXT,
+    Request_payload3 TEXT,
+    PRIMARY KEY (OperationName)
+);
